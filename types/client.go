f7886ab4672917d64229563e80aed148e9919f37
shandst <shandstas@gmail.com>
2024-05-14 17:25:05 +0300

    fix code

--- types/client.go
+++ types/client.go
@@ -24,16 +24,13 @@ type CustomTime struct {
 }
 
 func (ct *CustomTime) UnmarshalJSON(data []byte) error {
-	// Convert data to a string and remove surrounding quotes.
 	strInput := string(data)
-	strInput = strInput[1 : len(strInput)-1] // strip the surrounding quotes
+	strInput = strInput[1 : len(strInput)-1]
 
-	// If time ends with 'Z', it's in UTC; replace 'Z' with "+00:00" for Go's parsing compatibility
 	if strInput[len(strInput)-1] == 'Z' {
 		strInput = strInput[:len(strInput)-1] + "+00:00"
 	}
 
-	// Parse the time string assuming RFC3339 format, which includes the timezone
 	parsedTime, err := time.Parse(time.RFC3339, strInput)
 	if err != nil {
 		return fmt.Errorf("error parsing time: %w", err)
